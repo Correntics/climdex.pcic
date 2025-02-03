1387fbea1962cb10f5efd6f6dd45de4e52160deb
andreaangelidou <andrea.angelidou@correntics.com>
2025-02-03 14:57:55 +0100

    .fix(log): improve log
    .addresses https://github.com/Correntics/NEX-CMIP6-climate-indices/issues/15

--- R/climdex.r
+++ R/climdex.r
@@ -320,9 +320,7 @@ check.quantile.validity <- function(quantiles, present.vars, days.in.base) {
     stop("Provided quantiles must be a list.")
   
   if(!all(present.vars %in% names(quantiles)))
-    print("Missing variables in quantiles:")
-    print(setdiff(present.vars, names(quantiles)))  # Shows which are missing
-    stop("Quantiles must be present for all variables provided.\n")
+    stop(setdiff(present.vars, names(quantiles)))
 
   if(!all(sapply(quantiles[names(quantiles) %in% intersect(present.vars, c("tmax", "tmin"))], function(x) { "outbase" %in% names(x) && all(c("q10", "q90") %in% names(x$outbase)) })))
     stop("Temperature out-of-base quantiles must contain 10th and 90th percentiles.\n")
